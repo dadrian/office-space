4825bfdf886441dc6d3b036fc2b5bcd977a9edc0
David Adrian <davidcadrian@gmail.com>
2014-02-10 20:45:21 -0500

    Add Buildings class

---
+++ lib/mprint/buildings.py
@@ -0,0 +1,19 @@
+import mprint
+
+class Buildings(mprint.Client):
+
+    def __init__(self, credentials, *args, **kwargs):
+        super(Buildings, self).__init__(self, credentials)
+
+    def buildings(self):
+        method = 'GET'
+        path = '/buildings'
+        (res, buildings) = self.json_call(method, path)
+        return buildings
+
+    def building(self, id):
+        method = 'GET'
+        path = ''.join(['/buildings/', str(id)])
+        (res, building) = self.json_call(method, path)
+        return building
+        
